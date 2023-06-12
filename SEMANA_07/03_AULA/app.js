a6d2e210ec04ed869b127ab51fd05a43671bf0f0
Kakau17 <ana.martire@sou.inteli.edu.br>
2023-06-11 21:40:10 -0300

    atualização do repositório

---
+++ SEMANA_07/03_AULA/app.js
@@ -0,0 +1,47 @@
+const express = require('express'); 
+const bodyParser = require('body-parser');
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+const sqlite3 = require('sqlite3').verbose();
+const DBPATH = '../data/dbUser.db';
+const hostname = '127.0.0.1';
+const port = 3000;
+const app = express();
+
+app.get("/", urlencodedParser, (req, res) => {
+    res.header("Access-Control-Allow_Origin", "*");
+});
+
+app.get('/usuario', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	var sql = 'SELECT * FROM usuario ORDER BY nome COLLATE NOCASE';
+		db.all(sql, [],  (err, rows ) => {
+			if (err) {
+				throw err;
+			}
+			res.json(rows);
+		});
+		db.close(); // Fecha o banco
+});
+
+app.post('/insereUsuario', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	sql = "INSERT INTO usuario (nome, email, senha) VALUES ('" + req.body.nome + "', '" + req.body.email + "', " + req.body.senha + ")";
+	console.log(sql);
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}	
+	});
+	res.write('<p>USUARIO INSERIDO COM SUCESSO!</p><a href="/">VOLTAR</a>');
+	db.close(); // Fecha o banco
+	res.end();
+});
+
+app.listen(port, hostname, () => {
+    console.log(`Servidor rodando em http://${hostname}:${port}/`);
+  });
